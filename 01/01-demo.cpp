faec4188f24943c83c04779d766662f559c283a6
steve xiaohu zhao <steve_zhao@163.com>
2025-11-04 11:37:39 +0800

    cout and cin demo.

---
+++ 01/01-demo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int m, n;
+    cout << "Please enter tow numbers: " << endl;
+    cin >> m >> n;
+    cout << "The sum of " << m << " and " << n << " is " << (m + n) << endl;
+    return 0;
+}
